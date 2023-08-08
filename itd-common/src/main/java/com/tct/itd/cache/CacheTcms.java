98137ba9753797f0a7a0e2e92c3aadcf7ffee65d
liylongg <128231274+liylongg@users.noreply.github.com>
2023-08-08 22:52:09 +0800

    增加Cache模块

---
+++ itd-common/src/main/java/com/tct/itd/cache/CacheTcms.java
@@ -0,0 +1,34 @@
+package com.tct.itd.common.cache;
+
+/**
+ * @ClassName CacheTcms
+ * @Description TODO
+ * @Author YHF
+ * @Date 2022/5/7 16:52
+ */
+public class CacheTcms {
+    /**
+     * tcms数据空调实际温度
+     */
+    public static final String TCMS_AIR_ACTUAL = "TCMS:TCMS_AIR_ACTUAL";
+    /**
+     * tcms数据配置集合
+     */
+    public static final String TCMS_PACKET_DATA = "TCMS:TCMS_PACKET_DATA";
+    /**
+     * tcms空调数据
+     */
+    public static final String TCMS_AIR_TEMPERATURE = "TCMS:TCMS_AIR_TEMPERATURE";
+    /**
+     * tcms车门数据
+     */
+    public static final String TCMS_TRAIN_DOOR_STATE = "TCMS:TCMS_TRAIN_DOOR_STATE";
+    /**
+     * tcms通风机数据
+     */
+    public static final String TCMS_VENTILATION_STATE = "TCMS:TCMS_VENTILATION_STATE";
+    /**
+     * tcms数据发送序号
+     */
+    public static final String TCMS_SEND_SEQUENCE = "TCMS:TCMS_SEND_SEQUENCE";
+}
